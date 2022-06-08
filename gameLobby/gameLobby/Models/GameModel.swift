d7ab0c7d5d0ba4aeb21b08584125a4f944d3307b
frederickmo <frederick_0428@tongji.edu.cn>
2022-06-08 23:35:17 +0800

    图片url寄了，重新传了

--- gameLobby/gameLobby/Models/GameModel.swift
+++ gameLobby/gameLobby/Models/GameModel.swift
@@ -39,7 +39,7 @@ extension Game {
         Game(
             name: "NaughtyMonkey",
             displayName: "Naughty Monkey",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/naughty_monkey.jpg?sign=3ded738f4d89a83be1084f2b9d3def7d&t=1652961678",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/naughty%20monkey.jpeg?sign=274ca8e2c4cec408e39775c00e825bf8&t=1654702085",
             description: "简单刺激的动作类小游戏。点击屏幕操纵小猴在两棵大树之间跳跃，注意避开沿途的障碍物！",
             appPath: "",
             
@@ -48,7 +48,7 @@ extension Game {
         Game(
             name: "MikuDance",
             displayName: "与我共舞 Dancing Miku",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/dancing_miku.png?sign=315e21fdc853f2d54fe357a19124fa4e&t=1652961693",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/dancing%20miku.jpg?sign=e01a1f1c1e27b848921ebad15015442d&t=1654702120",
             description: "融合AR技术的全新音游呈现在你的面前！我们荣幸地邀请了Miku和她的小伙伴前来这场盛宴，您也想要参加这场精彩纷呈的舞会吗？",
             appPath: "DancingMiku://",
             category: ["单机", "音乐", "休闲"],
@@ -56,7 +56,7 @@ extension Game {
         Game(
             name: "HandTip",
             displayName: "HandTip",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/hand_tracking_rhythm_game.jpg?sign=2bb106638006c36267b8bb9a511b4872&t=1652961704",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/hand%20tip.jpg?sign=366fdc9149b17c88c9c708508985f3f0&t=1654702138",
             description: "融合手部识别技术的音乐游戏，让你体会节奏大师般畅爽体验。",
             appPath: "",
             category: ["单机", "音乐"],
@@ -64,7 +64,7 @@ extension Game {
         Game(
             name: "BombingMan",
             displayName: "炸弹人 Bombing Man",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/bomberman.jpg?sign=4461d0ed92d78620fb1d15d943e38da2&t=1652961714",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/bomberman.jpg?sign=6647de321b5cea8e1dba67f6e2ed07bf&t=1654702161",
             description: "儿时的炸弹人带来的无限美好，在此为你重新呈现。",
             appPath: "",
             category: ["在线", "竞技", "休闲"],
@@ -72,7 +72,7 @@ extension Game {
         Game(
             name: "",
             displayName: "QQ堂",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/qq%20tang.jpg?sign=71e3b2c1188feaaac03cba59d4bdeb8f&t=1653068049",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/qq%20tang.jpg?sign=4ec0535ac52b84303356f2ee85194ae7&t=1654702177",
             description: "",
             appPath: "",
             category: ["在线", "休闲", "竞技"],
@@ -80,7 +80,7 @@ extension Game {
         Game(
             name: "",
             displayName: "肥猫天使",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/twin_shot.png?sign=aeedc0e9108620eaf00f4b1c5ab77b24&t=1653067634",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/twin%20shot.png?sign=539f00c1a9b88e46c9a46aa232ea1681&t=1654702190",
             description: "",
             appPath: "",
             category: ["单机", "休闲", "动作"],
@@ -88,7 +88,7 @@ extension Game {
         Game(
             name: "",
             displayName: "冰激凌坏蛋",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/bad_ice_cream.png?sign=a11ef50a8817fab3898b1b5db5cad59d&t=1653067689",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/bad%20ice%20cream.jpeg?sign=9dcab1c75b3828da028a1878427998f3&t=1654702202",
             description: "",
             appPath: "",
             category: ["单机", "休闲", "动作"],
@@ -96,7 +96,7 @@ extension Game {
         Game(
             name: "",
             displayName: "神奇蓝药水",
-             previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/test_subject_arena.png?sign=f79455f12b58ef140b7231d9f9081c8a&t=1653067739",
+             previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/test%20subject%20arena.png?sign=49805f7ccfd5a20812c35c56c095340a&t=1654702212",
              description: "",
              appPath: "",
              category: ["单机", "休闲", "动作"],
@@ -104,7 +104,7 @@ extension Game {
         Game(
             name: "",
             displayName: "小鳄鱼爱洗澡",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/wheres_my_water.jpg?sign=a1b54f22c715bcdfe2bb496f7b0fd967&t=1653067769",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/wheres%20my%20water.jpg?sign=f4656b51dd0240d3963db5d5a98897fb&t=1654702222",
             description: "",
             appPath: "",
             category: ["单机", "休闲", "益智"],
@@ -112,7 +112,7 @@ extension Game {
         Game(
             name: "",
             displayName: "保卫萝卜",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/protect_carrot.png?sign=4bd28d00d9291269eec304715fc37f53&t=1653067871",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/carrot%20fantasy.jpeg?sign=a058682c85158009b743e63cd7aa6c71&t=1654702236",
             description: "",
             appPath: "",
             category: ["单机", "休闲", "益智", "策略"],
@@ -123,7 +123,7 @@ extension Game {
         Game(
             name: "NaughtyMonkey",
             displayName: "Naughty Monkey",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/naughty_monkey.jpg?sign=3ded738f4d89a83be1084f2b9d3def7d&t=1652961678",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/naughty%20monkey.jpeg?sign=274ca8e2c4cec408e39775c00e825bf8&t=1654702085",
             description: "简单刺激的动作类小游戏。点击屏幕操纵小猴在两棵大树之间跳跃，注意避开沿途的障碍物！",
             appPath: "",
             category: ["单机", "动作"],
@@ -131,7 +131,7 @@ extension Game {
         Game(
             name: "MikuDance",
             displayName: "与我共舞 Dancing Miku",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/dancing_miku.png?sign=315e21fdc853f2d54fe357a19124fa4e&t=1652961693",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/dancing%20miku.jpg?sign=e01a1f1c1e27b848921ebad15015442d&t=1654702120",
             description: "融合AR技术的全新音游呈现在你的面前！我们荣幸地邀请了Miku和她的小伙伴前来这场盛宴，您也想要参加这场精彩纷呈的舞会吗？",
             appPath: "DancingMiku://",
             category: ["单机", "音乐", "休闲"],
@@ -139,7 +139,7 @@ extension Game {
         Game(
             name: "HandTip",
             displayName: "HandTip",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/hand_tracking_rhythm_game.jpg?sign=2bb106638006c36267b8bb9a511b4872&t=1652961704",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/hand%20tip.jpg?sign=366fdc9149b17c88c9c708508985f3f0&t=1654702138",
             description: "融合手部识别技术的音乐游戏，让你体会节奏大师般畅爽体验。",
             appPath: "",
             category: ["单机", "音乐"],
@@ -147,7 +147,7 @@ extension Game {
         Game(
             name: "BombingMan",
             displayName: "炸弹人 Bombing Man",
-            previewImage: "https://636c-cloud1-0g0508tg0810d86e-1311181945.tcb.qcloud.la/exported%20data/ios%20game%20lobby/image/bomberman.jpg?sign=4461d0ed92d78620fb1d15d943e38da2&t=1652961714",
+            previewImage: "https://636c-cloud1-7ga8z75a2f0d1148-1309991354.tcb.qcloud.la/game%20cover/bomberman.jpg?sign=6647de321b5cea8e1dba67f6e2ed07bf&t=1654702161",
             description: "儿时的炸弹人带来的无限美好，在此为你重新呈现。",
             appPath: "",
             category: ["在线", "竞技", "休闲"],
